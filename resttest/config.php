d3d929098e5669abcea93e2fde6c4f981666d8c6
pchavali09 <35241780+pchavali09@users.noreply.github.com>
2018-02-14 22:07:22 -0500

    Initial Commit

---
+++ resttest/config.php
@@ -0,0 +1,6 @@
+<?php
+define("CLIENT_ID", "3MVG9snqYUvtJB1NnTa2aq7gwjnwH2EKjXOGBjSyte77iUUCQjGs2HXx2Qa3WgbD1qQJiFbx1rkaqDtWhQ0Tw");
+define("CLIENT_SECRET", "5360370222784616461");
+define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php");
+define("LOGIN_URI", "https://freeforcharity--V4SSite.cs50.my.salesforce.com");
+?>
